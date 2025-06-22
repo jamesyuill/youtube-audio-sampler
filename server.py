746c6d329e696f677f30f00d21e99f60f2e71450
James Yuill <jamesyuill@macbookpro.lan>
2025-06-22 17:01:37 +0100

    updates with more origins

--- server.py
+++ server.py
@@ -16,7 +16,9 @@ app = FastAPI()
 
 #need to change this for frontend obvs!!!
 origins = [
-    'http://127.0.0.1:5500'
+    'http://127.0.0.1:5500',
+    'http://localhost',
+    'http://localhost:5500'
 ]
 
 app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=['*'],allow_headers=['*'])
